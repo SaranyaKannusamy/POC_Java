16eebdde3b99502f0f78b69cdebda4d40daf7d68
SaranyaKannusamy <51782079+SaranyaKannusamy@users.noreply.github.com>
2019-07-01 12:17:13 +0530

    OAUTH

---
+++ boot-oauth2-password-resource-server/src/main/java/com/javainuse/Application.java
@@ -0,0 +1,16 @@
+package com.javainuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableResourceServer
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
